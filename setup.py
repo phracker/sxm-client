9f0bd9adcd83ce3613d625f34d5e8c5ea9e07cba
Christopher Bailey <cbailey@mort.is>
2019-05-18 19:16:36 -0400

    bump version

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     test_suite="tests",
     tests_require=requirements["dev"],
     url="https://github.com/AngellusMortis/sxm-client",
-    version="0.1.0",
+    version="0.1.1",
     zip_safe=False,
     extras_require={"dev": requirements["dev"]},
 )
