8d476ee4ade81626a13e1805ca20af3f40b69f9b
Christopher Bailey <cbailey@mort.is>
2021-07-15 11:09:00 -0400

    Combines CI and Release workflows

--- sxm/__init__.pyi
+++
@@ -1,2 +0,0 @@
-from sxm.client import AuthenticationError as AuthenticationError, HLS_AES_KEY as HLS_AES_KEY, SXMClient as SXMClient, SegmentRetrievalException as SegmentRetrievalException
-from sxm.http import make_http_handler as make_http_handler, run_http_server as run_http_server
--- sxm/cli.pyi
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class RegionChoice(str, Enum):
-    US: str
-    CA: str
-
-def main(username: str = ..., password: str = ..., do_list: bool = ..., port: int = ..., host: str = ..., verbose: bool = ..., region: RegionChoice = ...) -> int: ...
